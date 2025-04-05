2a2b9856e768c0280e3423708b73e1f861ccdc04
hav0x45 <pekkakukonlehto@outlook.com>
2025-04-06 02:11:15 +0300

    Main dev

--- eulerfunc.cpp
+++ eulerfunc.cpp
@@ -7,60 +7,38 @@ using namespace std;
 // Coprimes printing calculation not functioning perfectly,
 // that's for updating...
 
-void reverse(int arr[], int n){
-    reverse(arr,arr+n);
-}
-//function to print the array
-void printArray(int arr[], int n){
-     for(int i=0;i<n;i++)
-         cout << arr[i] << " ";
-}
-
 int euler(int number) {
 
     int result = number;
 
-    std::vector<int> cpvector;
     int cpiterator = 0;
 
     const int numberconst = number;
 
     if (number == 0) {
         result = number;
+        std::cout << "Euler function returns: ";
         return result;
     }
 
     if (number == 1) {
         result = number;
+        std::cout << "Euler function returns: ";
         return result;
     }
 
-    // TODO: Fix co primes 
     for (int iterator = 2; iterator * iterator <= number; iterator++) {
         if (number % iterator == 0) {
             for (cpiterator = 0; number % iterator == 0; cpiterator++) {
-                // Store coprimes in array
-                cpvector.push_back(number);
                 number = number / iterator;
             }
-            // At this point we should get the last coprime number, store it
-            cpvector.push_back(number);
+
             result = result - result / iterator;
         }
     }
 
-    // Reverse the order of coprime array
-    reverse(cpvector.begin(), cpvector.end());
-
     if (number > 1) {
         result = result - result / number;
-    }
-
-    std::cout << "Coprimes: " << std::endl;
-
-    // Print the coprimes 
-    for (int vectoriterator = 0; vectoriterator != cpvector.size(); ++vectoriterator) {
-        std::cout << vectoriterator + 1 << ": " << cpvector[vectoriterator] << std::endl;
     }
 
     std::cout << "Euler function returns: ";
