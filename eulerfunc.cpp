a7a9b10e3dd22b04f4e0a643aa065329b1079d4d
hav0x45 <pekkakukonlehto@outlook.com>
2025-04-09 11:46:21 +0300

    Some updates

--- eulerfunc.cpp
+++ eulerfunc.cpp
@@ -5,31 +5,15 @@
 using namespace std;
 
 int euler(int number) {
-
     int result = number;
-
     int cpiterator = 0;
-
-    const int numberconst = number;
-
-    if (number == 0) {
-        result = number;
-        std::cout << "Euler function returns: ";
-        return result;
-    }
-
-    if (number == 1) {
-        result = number;
-        std::cout << "Euler function returns: ";
-        return result;
-    }
 
     for (int iterator = 2; iterator * iterator <= number; iterator++) {
         if (number % iterator == 0) {
             for (cpiterator = 0; number % iterator == 0; cpiterator++) {
                 number = number / iterator;
+                
             }
-
             result = result - result / iterator;
         }
     }
@@ -48,7 +32,7 @@ int main(int argc, char* argv[]) {
     if (argc == 1) {
         std::cout << "No arguments passed." << std::endl;
     } else if (argc == 2) {
-       std::cout << euler(std::stoi(argv[1])) << std::endl;
+        std::cout << euler(stoi(argv[1])) << std::endl;
     } else {
         std::cout << "Syntax error." << std::endl;
     }
